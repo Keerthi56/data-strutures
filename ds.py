a6b0e318a87f7f027ded09ea28bf841f7fe5cd9e
Keerthi56 <64966874+Keerthi56@users.noreply.github.com>
2020-05-08 12:12:29 +0530

    Create ds.py

---
+++ ds.py
@@ -0,0 +1,17 @@
+#asssigning elements in a list
+list1=[1,2,3,4]
+list2=[5,6,7,8]
+print(list1+list2)
+
+c=5
+list1.append(c)
+print(list1)
+
+#accessing elements from a tuple
+tuple=(1,2,3,4)
+print(tuple[1],tuple[2])
+
+#deleting different dictionary elements
+dict={1:"one",2:"two",3:"three"}
+del dict[1]
+print(dict)
